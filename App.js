b92ab96adfa94ab1927039244ff9aa59a72c1709
gabuzera23 <gabrielroochha2005@gmail.com>
2024-10-07 10:56:41 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,59 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Chart as ChartJS, ArcElement, Title, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import GraphScreen from './screens/GraphScreen';
 
-// Registrando os componentes do Chart.js
-ChartJS.register(ArcElement, Title, Tooltip, Legend);
+const Stack = createStackNavigator();
 
 export default function App() {
-  const data = {
-    labels: ['Red', 'Blue', 'Yellow'],
-    datasets: [
-      {
-        label: 'Distribuição',
-        data: [300, 50, 100],
-        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
-        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      tooltip: {
-        callbacks: {
-          label: function (tooltipItem) {
-            return `Valor: ${tooltipItem.raw}`;
-          },
-        },
-      },
-    },
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Gráfico de Pizza</Text>
-      <Pie data={data} options={options} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen name="GraphScreen" component={GraphScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-});
